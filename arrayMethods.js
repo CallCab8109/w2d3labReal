d159dac31556548ae79154e77b3ed1014d109ffa
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-10-05 23:10:27 -0600

    finished pt2

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,9 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-// const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers  = mixedNumbers.filter(num => num % 2 === 0)
 
-
+// console.log(evenNumbers)
 
 ////////// PROBLEM 2 //////////
 
@@ -39,9 +39,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+ const postTaxPrices  = prices.map(num => num * 1.07);
 
-
+// console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
@@ -57,10 +57,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
-
-
+ const totalPopulation  = populations.reduce(function(acc, elem) {
+  return acc + elem
+ })
+// console.log(totalPopulation)
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -82,8 +82,16 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+function callback(monster) {
+  return monster.CP > 200
+}
 
+
+
+
+
+const myStrongest  = monstersInYourPocket.filter(callback)
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -100,9 +108,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
+const totPrice = orders.map(function(cost) {
+  return cost.price = (cost.price * cost.tax)
+})
 
-
-
+// console.log(totPrice)
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
@@ -120,3 +130,12 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+
+const bobsTotal = purchases
+    .filter(purchase => purchase.owner === "Bob")
+    .reduce((a, c) => a + c.price, 0)
+
+
+console.log(bobsTotal)
+
+//due to some circumstances beyond my control i couldn't work on this til very late and tried to learn from the solutions guie. i understand it but have made a note to come back again and try to solve these later
